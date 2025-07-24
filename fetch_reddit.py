42f2820b9b195588fe2d88c2c35364f36c9eca4a
giladscore494 <gameto818@gmail.com>
2025-07-25 02:11:35 +0300

    Update fetch_reddit.py

--- fetch_reddit.py
+++ fetch_reddit.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import praw
 import pandas as pd
+import datetime
 
-def fetch_reddit_posts(subreddits, limit=5):
+def fetch_reddit_posts(subreddits, days=7, limit=20):
     reddit = praw.Reddit(
         client_id=st.secrets["REDDIT_CLIENT_ID"],
         client_secret=st.secrets["REDDIT_CLIENT_SECRET"],
         user_agent=st.secrets["REDDIT_USER_AGENT"]
     )
 
+    cutoff = datetime.datetime.utcnow().timestamp() - days*24*60*60
     posts_data = []
     for sub in subreddits:
         subreddit = reddit.subreddit(sub)
-        for post in subreddit.hot(limit=limit):
-            posts_data.append({
-                "subreddit": sub,
-                "title": post.title,
-                "text": post.selftext,
-                "url": post.url
-            })
-
+        for post in subreddit.new(limit=limit):
+            if post.created_utc >= cutoff:
+                text = (post.title + " " + post.selftext).strip()
+                posts_data.append({
+                    "source": "Reddit",
+                    "title": post.title,
+                    "text": text,
+                    "url": post.url
+                })
     return pd.DataFrame(posts_data)
